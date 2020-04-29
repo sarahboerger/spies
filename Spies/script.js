f9caccc4bad73003bf16d99d445be1c66f0ed176
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2020-04-29 12:25:25 +0200

    oversat til dansk

--- Spies/script.js
+++ Spies/script.js
@@ -1,12 +1,12 @@
 function validateForm() {
   var name =  document.getElementById('name').value;
   if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
+      document.querySelector('.status').innerHTML = "Navn skal være udfyldt";
       return false;
   }
   var email =  document.getElementById('email').value;
   if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      document.querySelector('.status').innerHTML = "Email skal være udfyldt";
       return false;
   } else {
       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -17,12 +17,12 @@ function validateForm() {
   }
   var subject =  document.getElementById('subject').value;
   if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+      document.querySelector('.status').innerHTML = "Emne skal udfyldes";
       return false;
   }
   var message =  document.getElementById('message').value;
   if (message == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
+      document.querySelector('.status').innerHTML = "Din besked skal udfyldes";
       return false;
   }
   document.querySelector('.status').innerHTML = "Sending...";
@@ -53,3 +53,4 @@ error: function (jqXHR, textStatus, errorThrown)
 $('#status').text(jqXHR);
 }
 });
+
